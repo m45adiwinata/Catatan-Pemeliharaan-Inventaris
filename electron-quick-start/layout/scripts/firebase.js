4191f0780ea88af28169c2bffcd571ab1fb3f3d8
m45adiwinata <m45adiwinata@gmail.com>
2019-01-26 19:51:17 +0800

    First commit

---
+++ electron-quick-start/layout/scripts/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDK3U-0hNwJ6yphkYeemX2efd0aripEXnY",
+    authDomain: "data-kendaraan-pegawai.firebaseapp.com",
+    databaseURL: "https://data-kendaraan-pegawai.firebaseio.com",
+    projectId: "data-kendaraan-pegawai",
+    storageBucket: "data-kendaraan-pegawai.appspot.com",
+    messagingSenderId: "511282415446"
+};
+firebase.initializeApp(config);
